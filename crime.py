51b8da61a1c2c895c8900bfcf0d9fe6857893c9a
Theys Radmann <tm.radmann@radmann.cl>
2022-01-17 16:28:36 -0300

    V 1.6

--- crime.py
+++ crime.py
@@ -36,33 +36,33 @@ crimes = [
     r"heist",
     r"shoplift",
     r"siphon",
-    r"diverted|diversion", 
+    r"(diverted)|(diversion)", 
     # r"illicit enrichment",
     r"malversati",
     # r"public deposit",
     # r"illegally receiving public",
-    r"absorbing public deposits|funds",
+    r"absorbing public (deposits)|(funds)",
     r"illegal absorption of public",
-    r"illegally receiving|acquiring .*deposits",
-    r"illegally receiving|acquiring .*public",
-    r"illegally accepting|absorbing .*public",
-    r"illegally accepting|absorbing .*deposits",
+    r"illegally (receiving)|(acquiring) .*deposits",
+    r"illegally (receiving)|(acquiring) .*public",
+    r"illegally (accepting)|(absorbing) .*public",
+    r"illegally (accepting)|(absorbing) .*deposits",
     r"illicit enrichment",
     r"illegally obtain",
     r"illegally receiving pension",
     r"illegally receiving survivor benefits",
     # r"illegal gain",
-    r"funds .* illegally",
+    r"funds .*\s*illegally",
     # r"financial management irregularities",
     r"larceny",
-    r"ditribuit.* state .* asssets"
+    r"ditribuit.* state .*\s*asssets"
     # "Misappropriation of funds", included in misappropation
     # "Trafficking in Stolen Goods",
     # "Selling stolen goods",
     # "Receipt of stolen goods",
     # "Intellectual Property",
     ]
-words_apart = 11 # maximum distance of words apart from crime and conviction when matching cirme frst and conviction second
+words_apart = 35 # maximum distance of words apart from crime and conviction when matching cirme frst and conviction second
 # functions
 ############################################################
 def check_conviction(type, str_report, n):
@@ -80,7 +80,9 @@ def check_conviction(type, str_report, n):
         r"found guilty",
         r"imprisoned",
         r"fined",
-        r"arrested .+ serve"
+        r"arrested .+ serve",
+        r"ordered .*\s*to pay",
+        r"incarcerated",
     ]
     # keywords must be near crime type if before conv
     # build search string with crime type
@@ -88,48 +90,47 @@ def check_conviction(type, str_report, n):
     for str in phrase:
         # search keyword after conviction. Distance of words are not checked as crime usually follows conviction after a few words
         #  if specified after.
-        s_str = str + ' .* ' + type # RegEx word followed by space and anythnig in between and the second word
+        s_str = str + ' .*' + type # RegEx word followed by space and anythnig in between and the second word
         #_DEBUG print(n, s_str)
         #_DEBUG input("Press return")
         p = re.compile(s_str, re.IGNORECASE)
         x = p.search(str_report)
         if x:
             #_DEBUG print(n, x.group())
-            return True # exit fucntion for any crime found. these are most cases.
+            return True # exit function for any crime found. these are most cases.
         # if not found, check the other way around
-        s_str = type + ' .* ' + str
-        p = re.compile(s_str, re.IGNORECASE)
+        s_str = type + '.*? ' + str
+        p = re.compile(s_str, re.IGNORECASE)  
         x = p.search(str_report)
         if x:
             #_DEBUG print(n, x.group())
             # found. if there are too many words between the type and the conviction phrase, assume review
-            i_str = re.split("\s", x.group()) # split into words
-
+            i_str = re.split(r"\s", x.group()) # split into words
             if len(i_str) > words_apart:
-                print (n, " Too many words between crime and conv", end="\r")
+                print (n, "Too many words between crime and conv", end="\r")
                 post_conv = None # to flag for review
             else:
                 return True
             # end if
         else:
-            #_DEBUG print(n, str_report)
-            pass
+           pass
         # end if
     # end for
     return post_conv
 # end function ######################################################
 #####################################################################
-def check_issue(issues, str_Triage):
+def check_issue(issues, str_Triage, r):
 # checks if crime was found and convicted
 # # returns True (crime found and written in record), False, and None (to review) 
 #
 #####################################################################
     sic_crime = False
     for x_crime in issues:
-        s_crime = re.search(x_crime, str_Triage)
+        p = re.compile(x_crime, re.I) # to ignore case
+        s_crime = p.search(str_Triage)
         if s_crime:
             # found issue in string
-            # first, exclude identity theft and theft by deception #########
+            # exlusions
             if x_crime == "theft":
                 x = re.search("[Ii]dentity theft", str_Triage)
                 if x:
@@ -190,24 +191,24 @@ def check_issue(issues, str_Triage):
                     if y == None:
                         continue
             
-            # end exclude identity theft, theft by deception and stolen identity, etc ##########
+            # end exclusions
             # check conviction for crime
             chk = check_conviction(x_crime, str_Triage, r)
             if chk == None:
                 # too far away, flag for review (for now)
-                print(r, "SIC Review", end='\r')
+                print(r, "SIC Review              ", end='\r')
                 sic_crime = None
                 # we do not break as we could find a valid record for another kewword
             if chk == True:
                 # write correct to sheet
-                print(r, "   SIC Correct", end='\r')
+                print(r, "SIC Correct             ", end='\r')
                 ws.cell(row=r, column=15, value="SIC CORRECT")
                 return True
                 
         # end if
     # end for
     return sic_crime
-# end funcfions
+# end functions
 ###################################################
 
 # start program
@@ -259,7 +260,6 @@ for row in ws.rows:
     if c_OfficialLists:
         # extract lists from string
         # split string
-        #_DEBUG print(r, "List found")
         l_list = c_OfficialLists.split(';')
         i=0
         for tag in l_list:
@@ -271,42 +271,38 @@ for row in ws.rows:
             if x:
                 TagStr.append(x.group())
                  # we do not need to strip the brackets
-                print(r, "LIst match ", tag, "found", end="\r")
                 i += 1
                 Extra = True
             # end if
-    #   # end for
-        #_DEBUG print(TagStr)
+        # end for
     # end if
     # we now have TagInfo populated
-    # len(TagInfo) = mumber of elements (>0) or i , Ectra = True as flag, and i as the 
-    # for str in TagInfo:
-    #  we check crimes and convictions there as well at the end ofr the following loop
-    
-    # check for convvicted crimes in Report
-    sic_crime = check_issue(crimes, c_Triage)
+    # len(TagInfo) = mumber of elements (>0) or i , Extra = True as flag, and i as the number of element
+       
+    # check for convicted crimes in Report
+    sic_crime = check_issue(crimes, c_Triage, r)
     # now we check for Addidional List Tags
     if sic_crime == True:
-        continue # go to next record
+        continue # go to next record (check_isssue() writes correct SIC)
     if Extra:
         print("Checking additional in Lists", end="\r")
         for x_Triage in TagStr:
-            sic_crime = check_issue(crimes, x_Triage)
+            sic_crime = check_issue(crimes, x_Triage, r)
+            if sic_crime == True:
+                break
         # end for
     # end if
-    # if sic_crima was true, it was already written
+    # if sic_crime was true, it was already written
     if sic_crime == True:
-        continue
+        continue # not really necessary but in case more code comes below in loop...
     if sic_crime == False:
-        # SIC is not correct
-        #_DEBUG print(r, "No match: ", c_Reports)
-        print(r, "  SIC incorrect", end='\r')
+        print(r, "SIC incorrect                    ", end='\r')
         ws.cell(row=r, column=15, value="SIC INCORRECT")
     if sic_crime == None:
-        print(r, "Review manually", end='\r')
+        print(r, "Review manually                  ", end='\r')
         ws.cell(row=r, column=15, value="REVIEW MANUALLY")
-    # end loop thtough keywords 
-# end loop through rows
+   
+    # end loop through rows
 # write to new workbook
 
 print('\nWriting and saving results spreadsheet vacas4.xlsx ...')
